28a41936600f19e4a0cb6dce42bd1a1945425009
Pooja <poojachouksey10@gmail.com>
2021-10-12 22:35:04 +0530

    initial commit

---
+++ FileData/FileData/FileDetails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+    class FileDetails
+    {
+        public string Version(string version)
+        {
+            return "version 4.0";
+        }
+        public int Size(string size)
+        {
+            return 10;
+        }
+
+
+    }
+}
---
+++ FileData/FileData/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                string firstArg = args[0];
+                string secondArg = args[1];
+                FileDetails fileDetails = new FileDetails();
+
+                if (args[0] == "-v" || args[0] == "--v" || args[0] == "/v" || args[0] == "--version")
+                {
+                    string version = fileDetails.Version(secondArg);
+                    Console.WriteLine("Version is :" + version);
+                }
+
+                if (args[0] == "-s" || args[0] == "--s" || args[0] == "/s" || args[0] == "--size")
+                {
+                    int size = fileDetails.Size(secondArg);
+                    Console.WriteLine("Size is :" + size);
+                }
+
+                Console.ReadLine();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Program failed" + ex.Message);
+            }
+            
+
+
+           
+        }
+    }
+}
